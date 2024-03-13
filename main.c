efdef9aeb89041fec93952a19ce7dd0bc1858500
vancy12 <saanvishet12@gmail.com>
2024-03-13 23:36:38 +0530

    heap sort implemented

--- main.c
+++ main.c
@@ -17,11 +17,13 @@ int main() {
     int choice, data;
     do {
         printf("\nMenu:\n");
-        printf("1. Insert into BST\n");
+        printf("1. Insert\n");
         printf("2. Max heap\n");
         printf("3. Min Heap\n");
         printf("4. Level-wise Traversal\n");
-        printf("5. Exit\n");
+        printf("5. Max Heap Sort\n");
+        printf("6. Min Heap Sort\n");
+        printf("7. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -29,17 +31,17 @@ int main() {
             case 1:
                 printf("Enter data to insert: ");
                 scanf("%d", &data);
-                T1 = insertBST(T1, data);
+                T1 = insert(T1, data);
                 break;
             case 2:
                 printf("Converting to Max Heap... ");
                 T1 = maxHeap(T1);
-                printf("Call Level-wise Traversal to view max heap\n");
+                printf("Call Max Heap Sort to view max heap\n");
                 break;
             case 3:
                 printf("Converting to Min Heap... \n");
                 T1 = minHeap(T1);
-                printf("Call Level-wise Traversal to view max heap\n");
+                printf("Call Min Heap Sort to view min heap\n");
                 break;
 
             case 4:
@@ -48,15 +50,27 @@ int main() {
                 
                 printf("\n");
                 break;
-            
             case 5:
+                printf("Maximum heap Sort: ");
+                heapSortMax(T1);
+                
+                printf("\n");
+                break;
+            case 6:
+                printf("Minimum heap Sort: ");
+                heapSortMin(T1);
+                
+                printf("\n");
+                break;
+            
+            case 7:
                 free(T1.root);
                 printf("Exiting...\n");
                 break;
             default:
                 printf("Invalid choice! Please enter a number between 1 and 7.\n");
         }
-    } while(choice != 5);
+    } while(choice != 7);
  
     return 0;
 }
